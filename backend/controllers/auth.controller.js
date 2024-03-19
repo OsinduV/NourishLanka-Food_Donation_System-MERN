7a6d1c514cf9d5163bee64970c581d87ae5a3a26
Irushi Gunawaradana <irushigunawardana333@gmail.com>
2024-03-19 22:28:13 +0530

    Create signup api routes

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,36 @@
+import User from "../models/user.model.js"
+import bcryptjs from 'bcryptjs'// to encrypt the password
+
+export const signup = async (req,res) =>{
+   const { username,email,password } = req.body
+
+   if(
+    !username ||
+    !email || 
+    !password || 
+    username ===''|| 
+    email === '' || 
+    password === ''
+    ){
+    return res.status(400).json({messgae:'All fields are required'})
+   }
+
+   const hashedPassword = bcryptjs.hashSync(password,10)//encryption of the password
+
+   const newUser = new User({
+     username,
+     email,
+     password:hashedPassword
+   })
+
+   try{
+
+    await newUser.save()
+    res.json('Signup successfull' )
+
+   }catch(error){
+    res.status(500).json({message: error.message})
+   }
+
+   
+}
