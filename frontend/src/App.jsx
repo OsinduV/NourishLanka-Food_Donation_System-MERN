f07446b6985831177c21916460b84d8a5ac56cad
OsinduV <146355128+OsinduV@users.noreply.github.com>
2024-03-22 00:27:30 +0530

    create route for home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import Home from './pages/Home'
 
 export default function App() {
   return (
-    <h1 className="">App</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
