1563eec0f591559387959741b58ee8f028d7724f
Irushi Gunawaradana <irushigunawardana333@gmail.com>
2024-03-22 00:34:38 +0530

    Complete dark mode functionality

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,12 +2,15 @@ import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react'
 import React from 'react'
 import { Link,useLocation } from 'react-router-dom'// can go to pages without refreshing
 import{AiOutlineSearch} from 'react-icons/ai'//icons
-import{FaMoon} from 'react-icons/fa'
-import { useSelector } from 'react-redux'
+import{FaMoon,FaSun} from 'react-icons/fa'
+import { useSelector,useDispatch } from 'react-redux'
+import { toggleTheme } from '../redux/theme/themeSlice'
 
 export default function Header() {
     const path = useLocation().pathname;
+    const dispatch = useDispatch();
     const { currentUser } = useSelector((state) => state.user);
+    const { theme } = useSelector((state) => state.theme);
   return (
     
      <Navbar className='border-b-2'>
@@ -27,10 +30,18 @@ export default function Header() {
        <Button className='w-12 h-10 lg:hidden' color='gray' pill>
         <AiOutlineSearch/>
        </Button>
+
        <div className="flex gap-3 md:order-2">
-        <Button className='w-12 h-10 hidden sm:inline ' color='gray' pill>
-            <FaMoon />
-        </Button>
+
+       
+        <Button
+        className='w-12 h-10 hidden sm:inline'
+        color='gray'
+        pill
+        onClick={() => dispatch(toggleTheme())}
+      >
+        {theme === 'light' ? <FaSun /> : <FaMoon />}
+      </Button>
         {currentUser ? (
           <Dropdown
             arrowIcon={false}
