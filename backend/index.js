4c50e2acdbd4d26a9bb74d413baf9ba6b6f84f6d
OsinduV <146355128+OsinduV@users.noreply.github.com>
2024-03-31 03:38:36 +0530

    Add a middleware and a function to handle errors

--- backend/index.js
+++ backend/index.js
@@ -16,8 +16,18 @@ mongoose
 
 const app = express();
 
-app.use(express.json());//allow json as the input of the backend
+app.use(express.json()); //allow json as the input of the backend
 
 app.listen(process.env.PORT, () => {
-    console.log("Server is running on port ", process.env.PORT);
+  console.log("Server is running on port ", process.env.PORT);
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = err.ststusCode || 500; //why?500 : send an error without status code we're going to get an error
+  const message = err.message || "Internal server error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
   });
+});
