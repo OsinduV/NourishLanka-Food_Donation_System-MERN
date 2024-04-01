d37165e04acc4513e48287be782d49a78508e714
nesha02 <neshawickremasinghe@gmail.com>
2024-04-01 14:40:50 +0530

    file added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import Home from './pages/Home'
 import Header from './components/Header'
+import Volunteer_one from '../Volunteer/Volunteer_one'
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Header/>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/volunteerOne" element={<Volunteer_one/>}/>
       </Routes>
     </BrowserRouter>
   )
