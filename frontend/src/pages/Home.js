8933b29b724f9b890af491bc11bc6156ac14d510
Thenuka Wijetunga <thenuwije@gmail.com>
2024-04-10 15:17:17 +0530

    Refactor Home component to fetch inventory data

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -8,38 +8,34 @@ import InventoryForm from '../components/InventoryForm';
 const Home = () => {
     const { inventorys, inventory, dispatch} = useInventorysContext();
 
-    useEffect(() => {
-        const fetchInventorys = async () => {
-            try {
-                const response = await fetch('/api/inventorys');
-                if (response.ok) {
-                    const inventoryData = await response.json();
-                    dispatch({ type: 'SET_INVENTORYS', payload: inventoryData });
-                } else {
-                    console.error('Error fetching inventory data:', response.statusText);
-                }
-            } catch (error) {
-                console.error('Error fetching inventory data:', error);
+    const fetchInventorys = async () => {
+        try {
+            const response = await fetch('/api/inventorys');
+            if (response.ok) {
+                const inventoryData = await response.json();
+                dispatch({ type: 'SET_INVENTORYS', payload: inventoryData });
+            } else {
+                console.error('Error fetching inventory data:', response.statusText);
             }
-        };
+        } catch (error) {
+            console.error('Error fetching inventory data:', error);
+        }
+    }
 
+    useEffect(() => {
         fetchInventorys();
     }, [dispatch]);
 
     return (
         <div className="home">
-            {inventory ? (
-                <InventoryDetails inventory={inventory} />
-            ) : (
-                <>
-                    <div className="workouts">
-                        {inventorys && inventorys.map(inventory => (
-                            <InventoryDetails key={inventory._id} inventory={inventory} />
-                        ))}
-                    </div>
-                    <InventoryForm />
-                </>
-            )}
+            <>
+                <div className="workouts">
+                    {inventorys && inventorys.map(inventory => (
+                        <InventoryDetails key={inventory._id} inventory={inventory} />
+                    ))}
+                </div>
+                <InventoryForm />
+            </>
         </div>
     )
 };
