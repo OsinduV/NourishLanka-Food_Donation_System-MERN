e04813b1e0d7bc4d8bbab78fd444fc152e5e1956
nesha02 <neshawickremasinghe@gmail.com>
2024-04-21 17:02:19 +0530

    create get schedules API route

--- backend/Volunteer/controller/schedules.controller.js
+++ backend/Volunteer/controller/schedules.controller.js
@@ -32,4 +32,57 @@ export const create = async (req, res, next)=>{
       next(error);
     }
 
+};
+export const getschedules = async (req,res,next)=> {
+    try{
+
+        const startIndex = parseInt(req.query.startIndex) || 0;
+
+        //http://localhost:5000/api/schedules/getschedules?limit=1
+        const limit = parseInt(req.query.limit) || 9;  //9 is the number of schedules which we are requesting.
+
+        const sortDirection = req.query.order === 'asc' ? 1 : -1;
+
+        const schedules = await Schedule.find({
+            ...(req.query.userId && { userId: req.query.userId }),
+            ...(req.query.category && { category: req.query.category }),
+            ...(req.query.day && { status: req.query.day }),
+            ...(req.query.slug && { slug: req.query.slug }),
+            ...(req.query.scheduleId && { _id: req.query.scheduleId }),
+            ...(req.query.searchTerm && {
+                //using or it alows us to search between two places
+              $or: [ 
+                { catagory: { $regex: req.query.searchTerm, $options: 'i' } }, //by regex tool it allows to search inside the title and from i, it tells that lowecase or uppercase is not important
+                { day: { $regex: req.query.searchTerm, $options: 'i' } },
+              ],
+        }),
+    })   .sort({ updatedAt: sortDirection })
+    .skip(startIndex)
+    .limit(limit);
+    
+    //count the total number of schedules
+    const totalschedules = await Schedule.countDocuments();
+
+    //to count schedules on last month ( from today to last month )
+    const oneMonthAgo = new Date();
+    
+    oneMonthAgo.setMonth(oneMonthAgo.getMonth() - 1);
+
+    //last months schedules
+    const lastMonthschedules = await Schedule.countDocuments({
+        createdAt: { $gte: oneMonthAgo },
+      });
+
+      //response
+      res.status(200).json({
+        schedules ,
+        totalschedules,
+        lastMonthschedules,
+      });
+  
+
+
+    }catch(error){
+        next(error);
+    }
 }
--- backend/Volunteer/route/schedules.route.js
+++ backend/Volunteer/route/schedules.route.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { verifyToken } from '../../utills/verifyUser.js';
 import { create } from '../controller/schedules.controller.js';
+import { getschedules } from '../controller/schedules.controller.js';
 
 const router = express.Router();
 
 router.post('/create',verifyToken,create)
+router.get('/getschedules',getschedules)
 
 export default router;
