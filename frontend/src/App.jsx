b0b4c27ed06606d3799f601ab6099618e6c7f5ac
DragonPro001 <147476130+DragonPro001@users.noreply.github.com>
2024-04-24 00:54:50 +0530

    frontend page links-app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,15 @@ import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
 import Project from "./pages/Project"
 import Dashboard from "./pages/Dashboard"
+import FbRegisterpage from "./pages/foodbank_register"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Header from "./components/Header"
 import Footer from './components/Footer'
 import PrivateRoute from "./components/PrivateRoute"
+import Fbhome from './pages/foodbank_home'
+import FoodbankDash from "./pages/foodbankDash"
+
 
 export default function App() {
   return (
@@ -21,11 +25,16 @@ export default function App() {
         <Route path="/about" element={<About/>} />
         <Route path="/sign-in" element={<SignIn/>} />
         <Route path="/sign-up" element={<SignUp/>} />
-        <Route element={<PrivateRoute />}>
-          <Route path='/dashboard' element={<Dashboard />} />
-        </Route>
-        <Route path="/projects" element={<Project/>} />
+        <Route path='/foodbankreg' element={<FbRegisterpage />} />
+        <Route path='/foodbankhome' element={<Fbhome />} />
+        <Route path='/foodbankDash' element={<FoodbankDash />} />
         
+         <Route element={<PrivateRoute />}>
+        <Route path='/dashboard' element={<Dashboard />} />
+         
+        </Route>
+        <Route path="/projects" element={<Project/>} />        
+
       </Routes>
       <Footer/>
     </BrowserRouter>
