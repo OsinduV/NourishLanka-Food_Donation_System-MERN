4a1c1404eb6199defdc335d29b1cc973389c6e8e
DragonPro001 <147476130+DragonPro001@users.noreply.github.com>
2024-04-24 00:53:59 +0530

    food bank routes

--- backend/routes/foodbank.route.js
+++ backend/routes/foodbank.route.js
@@ -2,7 +2,7 @@ import express, { Router } from 'express'
 import { register,readallfb,updatefb,deletefb,searchfb,statuschange } from '../controllers/foodbank.controller.js';
 
 const router =  express.Router();
-
+//routes related to food bank
 router.post('/register',register); 
 router.get('/readallfb',readallfb);
 router.put('/updatefb/:id',updatefb);
