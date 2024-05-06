105e7bb79dd5f87c38059a70c796c1e3eb538714
Irushi Gunawaradana <irushigunawardana333@gmail.com>
2024-05-06 19:29:55 +0530

    Adding sort and search to dashadmin in community

--- frontend/src/CommunityManagement/components/CallToAction.jsx
+++ frontend/src/CommunityManagement/components/CallToAction.jsx
@@ -1,25 +1,32 @@
-import { Button } from 'flowbite-react';
+import { Button } from "flowbite-react";
 
 export default function CallToAction() {
   return (
-    <div className='flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center'>
-        <div className="flex-1 justify-center flex flex-col">
-            <h2 className='text-2xl'>
-            Want to join our cause to fight hunger and ensure everyone has enough to eat?
+    <div className="flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center">
+      <div className="flex-1 justify-center flex flex-col">
+        <h2 className="text-2xl">
+          Want to join our cause to fight hunger and ensure everyone has enough
+          to eat?
+        </h2>
+        <p className="text-gray-500 my-2">
+          Be a beacon of hope by extending a meal to those in need
+        </p>
 
-            </h2>
-            <p className='text-gray-500 my-2'>
-            Be a beacon of hope by extending a meal to those in need
-            </p>
-            <Button gradientDuoTone='greenToBlue' className='rounded-tl-xl rounded-bl-none'>
-                <a href="/fr-home" target='_blank' rel='noopener noreferrer'>
-                    Donate Now
-                </a>
+        <div className="flex justify-center">
+          <a href="/fr-home">
+            <Button
+              gradientDuoTone="greenToBlue"
+              className="rounded-tl-xl rounded-bl-none"
+              style={{ width: "200px" }} // Adjust the width as needed
+            >
+              Donate Now
             </Button>
+          </a>
         </div>
-        <div className="p-7 flex-1">
-            <img src="https://www.thefoodcloset.org/uploads/2/2/4/3/22433308/4_orig.png" />
-        </div>
+      </div>
+      <div className="p-7 flex-1">
+        <img src="https://www.thefoodcloset.org/uploads/2/2/4/3/22433308/4_orig.png" />
+      </div>
     </div>
-  )
+  );
 }
